215343c137e1e86d8f724e5d1ccf82d32f7f66ec
Majid Qureshi <mpq1990@gmail.com>
2020-04-11 19:23:59 +0500

    chore: add bootstrap

--- src/components/nav-bar/navBar.js
+++ src/components/nav-bar/navBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core'
+import Navbar from "react-bootstrap/Navbar";
 
 
 const NavBar = () => {
@@ -27,9 +28,18 @@ const NavBar = () => {
     `;
 
     return (
-    <header css={navBarStyle}>
-        <a css={ logoStyle }
-        href="/">COVID-19</a>
-    </header>)
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+            <img
+                alt=""
+                src="/logo.svg"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+            />{' '}
+            COVID-19
+            </Navbar.Brand>
+        </Navbar>
+    )
 }
 export default NavBar;

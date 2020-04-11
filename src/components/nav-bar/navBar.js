25d2cceb9189f36643c9d5fca89eafa82da1cc1d
Majid Qureshi <mpq1990@gmail.com>
2020-04-11 19:32:53 +0500

    add: prettier, eslint

--- src/components/nav-bar/navBar.js
+++ src/components/nav-bar/navBar.js
@@ -1,45 +1,44 @@
-import React from "react";
+import React from 'react';
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core'
-import Navbar from "react-bootstrap/Navbar";
-
+import { jsx, css } from '@emotion/core';
+import Navbar from 'react-bootstrap/Navbar';
 
 const NavBar = () => {
-    const navBarStyle = css `
-        display: flex;
-        align-content: center;
-        justify-content: space-between;
-        width: 100%;
-        margin: 0 auto;
-        padding: 20px 0px 0px 0px;
-    `;
+  const navBarStyle = css`
+    display: flex;
+    align-content: center;
+    justify-content: space-between;
+    width: 100%;
+    margin: 0 auto;
+    padding: 20px 0px 0px 0px;
+  `;
 
-    const logoStyle = css `
-        color: #bf3334;
-        font-size: 32px;
-        text-decoration: none;
-        font-weight: bold;
-        padding-bottom: 10px;
-        display: flex;
-        width: 279px;
-        height: 76px;
-        margin-left: auto;
-        margin-right auto;
-    `;
+  const logoStyle = css`
+    color: #bf3334;
+    font-size: 32px;
+    text-decoration: none;
+    font-weight: bold;
+    padding-bottom: 10px;
+    display: flex;
+    width: 279px;
+    height: 76px;
+    margin-right: auto;
+    margin-left: auto;
+  `;
 
-    return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="#home">
-            <img
-                alt=""
-                src="/logo.svg"
-                width="30"
-                height="30"
-                className="d-inline-block align-top"
-            />{' '}
-            COVID-19
-            </Navbar.Brand>
-        </Navbar>
-    )
-}
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        COVID-19
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
 export default NavBar;
--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,29 +1,31 @@
-import React from "react";
+import React from 'react';
 /** @jsx jsx */
-import { jsx, css } from '@emotion/core'
+import { jsx, css } from '@emotion/core';
 
 const Summary = ({ figures }) => {
   const ulStyle = css`
     list-style-type: none;
     display: flex;
     flex-direction: column;
-    margin: 0; /* To remove default bottom margin */ 
+    margin: 0; /* To remove default bottom margin */
     padding: 0; /* To remove default left padding */
-  `
+  `;
 
-  const liStyle = css `
+  const liStyle = css`
     margin-bottom: 10px;
-  `
+  `;
 
-  return (<ul css={ulStyle}>
-    {Object.keys(figures).map((key, index) => {
-      return (
-        <li key= {key} css={liStyle}>
-          {key}: {figures[key]}
-        </li>
-      );
-    })}
-  </ul>)
+  return (
+    <ul css={ulStyle}>
+      {Object.keys(figures).map((key, index) => {
+        return (
+          <li key={key} css={liStyle}>
+            {key}: {figures[key]}
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default Summary;

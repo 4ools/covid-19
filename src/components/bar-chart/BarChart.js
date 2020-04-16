7d6b0a27db4acc2008ff6086844cda679655fefb
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-04-16 21:57:42 +0200

    just some tidying up

--- src/components/bar-chart/BarChart.js
+++ src/components/bar-chart/BarChart.js
@@ -33,7 +33,7 @@ const BarChart = ({ data, titleStart, titleEnd, pick, options }) => {
           data={data}
           keys={keys.slice(1, keys.length)}
           indexBy="country"
-          margin={{ top: 50, right: 130, bottom: 50, left: 80 }}
+          margin={{ top: 50, right: 50, bottom: 50, left: 80 }}
           padding={0.3}
           colors={getColor}
           borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
@@ -58,30 +58,6 @@ const BarChart = ({ data, titleStart, titleEnd, pick, options }) => {
           labelSkipWidth={12}
           labelSkipHeight={12}
           labelTextColor={{ from: 'color', modifiers: [['brighter', 1.6]] }}
-          legends={[
-            {
-              dataFrom: 'keys',
-              anchor: 'bottom-right',
-              direction: 'column',
-              justify: false,
-              translateX: 120,
-              translateY: 0,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: 'left-to-right',
-              itemOpacity: 0.85,
-              symbolSize: 20,
-              effects: [
-                {
-                  on: 'hover',
-                  style: {
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
           animate
           motionStiffness={90}
           motionDamping={15}
--- src/components/nav-bar/navBar.js
+++ src/components/nav-bar/navBar.js
@@ -2,13 +2,21 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 
 const NavBar = () => {
   return (
     <AppBar position="static" color="primary">
-      <Toolbar>
-        <Typography variant="h6">COVID-19</Typography>
-      </Toolbar>
+      <Container maxWidth="md">
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Toolbar style={{ padding: 0 }}>
+              <Typography variant="h6">COVID-19</Typography>
+            </Toolbar>
+          </Grid>
+        </Grid>
+      </Container>
     </AppBar>
   );
 };
